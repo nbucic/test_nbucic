18d39443b0b467fd6abb1e22d7b297f839a995da
nbucic <nikola.bucic@gmail.com>
2013-10-17 21:37:40 +0200

    2

--- index.php
+++ index.php
@@ -3,12 +3,21 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+$print_r_pre = function ($elem) {
+   print "<pre>";
+   print_r($elem);
+   print "</pre>";
+};
+
 $app->get('/hello/:name', function ($name) {
    echo "Hello, $name";
 });
 
-$app->get('/', function () {
-   echo "TEST1!";
+$app->get('/info/', function () use ($app, $print_r_pre) {
+   print "<br>server_address: {$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}/<br>";
+   $print_r_pre($app);
+   print "<br><br>";
+   print "EOM";
 });
 
 $app->get('/test/', function () {
